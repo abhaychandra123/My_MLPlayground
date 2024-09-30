7c8e80fa9941f87e7f9e2c5835a29f71d6ee6743
abhaychandra123 <71028688+abhaychandra123@users.noreply.github.com>
2024-09-30 22:45:28 +0530

    Updated kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,28 +1,21 @@
-import csv
-import random
-import numpy as np
-import pandas
-import matplotlib.pyplot as plt
+#import csv
 
-fields = []
-rows = []
-
-file="data-2.csv"
-with open(file,"r") as f:
-    read=csv.reader(f)
-    fields=next(read)
-    # prfloat(type(read))
-    # prfloat(type(read[0])) #TypeError: '_csv.reader' object is not subscriptable
+# file="data-2.csv"
+# with open(file,"r") as f:
+#     read=csv.reader(f)
+#     fields=next(read)
+#     # prfloat(type(read))
+#     # prfloat(type(read[0])) #TypeError: '_csv.reader' object is not subscriptable
 
 
-    for row in read:
-        row.pop()
-        rows.append(row)
+#     for row in read:
+#         row.pop()
+#         rows.append(row)
 
 
-x=np.array([i[0] for i in rows])
-y=np.array([i[1] for i in rows])
-# print(y[0:5])
+# x=np.array([i[0] for i in rows])
+# y=np.array([i[1] for i in rows])
+# # print(y[0:5])
 
 # print(x[0:5])
 # plt.scatter(x,y,color='green')
@@ -30,28 +23,41 @@ y=np.array([i[1] for i in rows])
 # plt.ylim(min(y), max(y))
 # plt.show()
 
-df  = pandas.read_csv("data-3.csv")
-  # plots all columns against index
-df.plot(kind='scatter',x='x',y='y',color="red") 
-plt.show()
-
-
-k=3
-clustercentroid=[]
-PtsOfCent=[]
-for i in range(k):
-    clustercentroid.append(random.choice(rows))
-
-ccx=np.array([i[0] for i in clustercentroid])
-ccy=np.array([i[1] for i in clustercentroid])   
-
-# plt.scatter(ccx,ccy,color='green')
-# plt.xlim(min(x), max(x))
-# plt.ylim(min(y), max(y))
-# plt.show()
-
-plt.show()
-
-print(clustercentroid)
-
 # def elud_dist
+#import required libraries
+import sys
+from skimage import io
+from sklearn.cluster import KMeans
+import numpy as np
+
+# reading filename
+
+filename = sys.argv[1]
+
+# reading the image
+image = io.imread(filename)
+ 
+# preprocessing
+rows, cols = image.shape[0], image.shape[1]
+image = image.reshape(rows * cols, 3)
+
+# modelling
+print('Compressing...')
+kMeans = KMeans(n_clusters = 16)
+kMeans.fit(image)
+
+# getting centers and labels
+centers = np.asarray(kMeans.cluster_centers_, dtype=np.uint8)
+labels = np.asarray(kMeans.labels_, dtype = np.uint8)
+labels = np.reshape(labels, (rows, cols))
+print('Almost done.')
+
+# reconstructing the image
+newImage = np.zeros((rows, cols, 3), dtype=np.uint8)
+for i in range(rows):
+    for j in range(cols):
+            # assinging every pixel the rgb color of their label's center
+            newImage[i, j, :] = centers[labels[i, j], :]
+io.imsave(filename.split('.')[0] + '-compressed.png', newImage)
+
+print('Image has been compressed sucessfully.')
